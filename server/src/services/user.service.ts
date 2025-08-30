7bd10a9e57471a215363dbe0417ab9e17dc47d64
delfin <carljefferson.delfin@gmail.com>
2025-08-30 18:16:38 +0800

    add user service

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,14 @@
+import UserRepository from "@/repositories/user.repository";
+
+class UserService {
+  private userRepository: UserRepository;
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async createUser(email: string) {
+    return this.userRepository.createUser(email);
+  }
+}
+
+export default UserService;
