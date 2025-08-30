6f17975b0e6ba6ce9649412de87815fbbad5e9da
delfin <carljefferson.delfin@gmail.com>
2025-08-30 18:19:04 +0800

    create user controller

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { z } from "zod";
+import UserService from "@/services/user.service";
+
+const createUserSchema = z.object({
+  email: z.string().email(),
+});
+
+const userService = new UserService();
+
+export const createUserController = async (req: Request, res: Response) => {
+  try {
+    const validation = createUserSchema.safeParse(req.body);
+
+    if (!validation.success) {
+      return res.status(400).json({ error: validation.error });
+    }
+
+    const { email } = validation.data;
+
+    const user = await userService.createUser(email);
+
+    return res.status(201).json({ message: "User created", data: user });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
