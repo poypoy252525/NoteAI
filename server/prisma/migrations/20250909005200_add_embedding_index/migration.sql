4dbbcf5a18c2a18fd0f15681c6119403272a3256
delfin <carljefferson.delfin@gmail.com>
2025-09-09 02:10:21 +0800

    swap to ivfflat

--- server/prisma/migrations/20250909005200_add_embedding_index/migration.sql
+++ server/prisma/migrations/20250909005200_add_embedding_index/migration.sql
@@ -1,2 +1,2 @@
 -- CreateIndex
-CREATE INDEX IF NOT EXISTS "note_embedding_idx" ON "Note" USING hnsw (embedding vector_cosine_ops) WITH (lists = 100);
+CREATE INDEX IF NOT EXISTS "note_embedding_idx" ON "Note" USING ivfflat (embedding vector_cosine_ops) WITH (lists = 100);
