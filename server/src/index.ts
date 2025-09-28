f402ed00ccecad3644e302d8f27817b138aae11c
delfin <carljefferson.delfin@gmail.com>
2025-09-29 02:20:08 +0800

    add rate limit

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,20 @@ import express from "express";
 import router from "./routes";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later.",
+});
 
 const app = express();
 
 app.use(cors({ origin: process.env.ALLOWED_ORIGIN!, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.json("Welcome to the NoteAI API!");
